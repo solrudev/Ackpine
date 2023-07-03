b03e670057b8dd18368dd91e3f1efea1c14282fa
Ilya Fomichev <fomil.music@gmail.com>
2023-07-03 17:10:17 +0500

    add info about permissions to InstallerType docs

--- ackpine-core/src/main/kotlin/ru/solrudev/ackpine/installer/parameters/InstallerType.kt
+++ ackpine-core/src/main/kotlin/ru/solrudev/ackpine/installer/parameters/InstallerType.kt
@@ -1,29 +1,35 @@
 package ru.solrudev.ackpine.installer.parameters
 
-import android.content.Intent
+import android.content.Intent.ACTION_INSTALL_PACKAGE
 import android.content.pm.PackageInstaller
 import android.os.Build
 import androidx.annotation.ChecksSdkIntAtLeast
 import androidx.annotation.RequiresApi
 import ru.solrudev.ackpine.installer.parameters.InstallerType.INTENT_BASED
 import ru.solrudev.ackpine.installer.parameters.InstallerType.SESSION_BASED
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.REQUEST_INSTALL_PACKAGES
 
 /**
  * Type of the package installer implementation.
  *
- * * [INTENT_BASED] &mdash; package installer will use the [Intent.ACTION_INSTALL_PACKAGE] intent action to install the
+ * * [INTENT_BASED] &mdash; package installer will use the [ACTION_INSTALL_PACKAGE] intent action to install the
  * 	 package.
  * * [SESSION_BASED] &mdash; package installer will use system's [PackageInstaller] API to install the package.
  */
 public enum class InstallerType {
 
 	/**
-	 * Package installer will use the [Intent.ACTION_INSTALL_PACKAGE] intent action to install the package.
+	 * Package installer will use the [ACTION_INSTALL_PACKAGE] intent action to install the package.
+	 *
+	 * Requires [READ_EXTERNAL_STORAGE] and [REQUEST_INSTALL_PACKAGES] permissions.
 	 */
 	INTENT_BASED,
 
 	/**
 	 * Package installer will use system's [PackageInstaller] API to install the package.
+	 *
+	 * Requires [REQUEST_INSTALL_PACKAGES] permission.
 	 */
 	@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 	SESSION_BASED;
