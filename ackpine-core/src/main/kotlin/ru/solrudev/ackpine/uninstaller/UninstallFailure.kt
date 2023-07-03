fd7ddcb9566bc2ea6e289a9de62fa22f2484da08
Ilya Fomichev <fomil.music@gmail.com>
2023-07-03 17:36:36 +0500

    add UninstallFailure.Aborted

--- ackpine-core/src/main/kotlin/ru/solrudev/ackpine/uninstaller/UninstallFailure.kt
+++ ackpine-core/src/main/kotlin/ru/solrudev/ackpine/uninstaller/UninstallFailure.kt
@@ -11,6 +11,12 @@ public sealed interface UninstallFailure : Failure, Serializable {
 	public data object Generic : UninstallFailure
 
 	/**
+	 * The operation failed because it was actively aborted.
+	 * For example, the user actively declined uninstall request.
+	 */
+	public data class Aborted(public val message: String) : UninstallFailure
+
+	/**
 	 * The operation failed because an exception was thrown.
 	 */
 	public data class Exceptional(public override val exception: Exception) : UninstallFailure, Failure.Exceptional
