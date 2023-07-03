7a7a23525626ae826e88dcc17d8b464cbe08c8be
Ilya Fomichev <fomil.music@gmail.com>
2023-07-03 13:05:09 +0500

    fix progress flow extension

---
+++ ackpine-ktx/src/main/kotlin/ru/solrudev/ackpine/session/SessionProgress.kt
@@ -0,0 +1,22 @@
+package ru.solrudev.ackpine.session
+
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.conflate
+import ru.solrudev.ackpine.DisposableSubscriptionContainer
+import ru.solrudev.ackpine.plusAssign
+
+public val <F : Failure> ProgressSession<F>.progress: Flow<Progress>
+	get() = callbackFlow {
+		val subscriptionContainer = DisposableSubscriptionContainer()
+		subscriptionContainer += addProgressListener { _, progress ->
+			trySend(progress)
+		}
+		subscriptionContainer += addStateListener { _, state ->
+			if (state.isTerminal) {
+				channel.close()
+			}
+		}
+		awaitClose(subscriptionContainer::dispose)
+	}.conflate()
