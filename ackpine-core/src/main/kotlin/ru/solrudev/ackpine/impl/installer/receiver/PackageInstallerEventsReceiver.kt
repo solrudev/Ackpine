73a799d115deb1d85bd41c2e26d9b3f3ecae9416
Ilya Fomichev <fomil.music@gmail.com>
2023-07-04 12:52:50 +0500

    get session in PackageInstallerEventsReceiver asynchronously

--- ackpine-core/src/main/kotlin/ru/solrudev/ackpine/impl/installer/receiver/PackageInstallerEventsReceiver.kt
+++ ackpine-core/src/main/kotlin/ru/solrudev/ackpine/impl/installer/receiver/PackageInstallerEventsReceiver.kt
@@ -5,8 +5,10 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageInstaller
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.annotation.RestrictTo
+import ru.solrudev.ackpine.helpers.handleResult
 import ru.solrudev.ackpine.impl.activity.LauncherActivity
 import ru.solrudev.ackpine.impl.installer.activity.SessionBasedInstallConfirmationActivity
 import ru.solrudev.ackpine.impl.installer.receiver.helpers.getParcelableExtraCompat
@@ -21,40 +23,61 @@ import ru.solrudev.ackpine.installer.PackageInstaller as AckpinePackageInstaller
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 internal class PackageInstallerEventsReceiver : BroadcastReceiver() {
 
+	private lateinit var pendingResult: PendingResult
+
 	@Suppress("UNCHECKED_CAST")
 	override fun onReceive(context: Context, intent: Intent) {
 		if (intent.action != getAction(context)) {
 			return
 		}
+		pendingResult = goAsync()
 		val packageInstaller = AckpinePackageInstaller.getInstance(context)
 		val ackpineSessionId = intent.getSerializableExtraCompat<UUID>(LauncherActivity.SESSION_ID_KEY)!!
-		val session = packageInstaller.getSessionAsync(ackpineSessionId).get() as? CompletableSession<InstallFailure>
-		val sessionId = intent.getIntExtra(PackageInstaller.EXTRA_SESSION_ID, -1)
-		val status = intent.getIntExtra(PackageInstaller.EXTRA_STATUS, -1)
-		if (status == PackageInstaller.STATUS_PENDING_USER_ACTION) {
-			val confirmationIntent = intent.getParcelableExtraCompat<Intent>(Intent.EXTRA_INTENT)
-			if (confirmationIntent != null) {
-				val wrapperIntent = Intent(context, SessionBasedInstallConfirmationActivity::class.java)
-					.putExtra(Intent.EXTRA_INTENT, confirmationIntent)
-					.putExtra(PackageInstaller.EXTRA_SESSION_ID, sessionId)
-					.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-				context.startActivity(wrapperIntent)
-			} else {
-				session?.completeExceptionally(
-					IllegalArgumentException("InstallationEventsReceiver: confirmationIntent was null.")
-				)
+		try {
+			packageInstaller.getSessionAsync(ackpineSessionId).handleResult { session ->
+				handlePackageInstallerStatus(session as? CompletableSession<InstallFailure>, intent, context)
 			}
-			return
+		} catch (t: Throwable) {
+			pendingResult.finish()
+			Log.e("InstallerEventsReceiver", null, t)
 		}
-		val result = if (status == PackageInstaller.STATUS_SUCCESS) {
-			Session.State.Succeeded
-		} else {
-			val message = intent.getStringExtra(PackageInstaller.EXTRA_STATUS_MESSAGE)
-			val otherPackageName = intent.getStringExtra(PackageInstaller.EXTRA_OTHER_PACKAGE_NAME)
-			val storagePath = intent.getStringExtra(PackageInstaller.EXTRA_STORAGE_PATH)
-			Session.State.Failed(InstallFailure.fromStatusCode(status, message, otherPackageName, storagePath))
+	}
+
+	private fun handlePackageInstallerStatus(
+		session: CompletableSession<InstallFailure>?,
+		intent: Intent,
+		context: Context
+	) {
+		try {
+			val sessionId = intent.getIntExtra(PackageInstaller.EXTRA_SESSION_ID, -1)
+			val status = intent.getIntExtra(PackageInstaller.EXTRA_STATUS, -1)
+			if (status == PackageInstaller.STATUS_PENDING_USER_ACTION) {
+				val confirmationIntent = intent.getParcelableExtraCompat<Intent>(Intent.EXTRA_INTENT)
+				if (confirmationIntent != null) {
+					val wrapperIntent = Intent(context, SessionBasedInstallConfirmationActivity::class.java)
+						.putExtra(Intent.EXTRA_INTENT, confirmationIntent)
+						.putExtra(PackageInstaller.EXTRA_SESSION_ID, sessionId)
+						.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+					context.startActivity(wrapperIntent)
+				} else {
+					session?.completeExceptionally(
+						IllegalArgumentException("PackageInstallerEventsReceiver: confirmationIntent was null.")
+					)
+				}
+				return
+			}
+			val result = if (status == PackageInstaller.STATUS_SUCCESS) {
+				Session.State.Succeeded
+			} else {
+				val message = intent.getStringExtra(PackageInstaller.EXTRA_STATUS_MESSAGE)
+				val otherPackageName = intent.getStringExtra(PackageInstaller.EXTRA_OTHER_PACKAGE_NAME)
+				val storagePath = intent.getStringExtra(PackageInstaller.EXTRA_STORAGE_PATH)
+				Session.State.Failed(InstallFailure.fromStatusCode(status, message, otherPackageName, storagePath))
+			}
+			session?.complete(result)
+		} finally {
+			pendingResult.finish()
 		}
-		session?.complete(result)
 	}
 
 	internal companion object {
