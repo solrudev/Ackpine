d5fa4bcb079a2c7e3fd0886eaf9581c867f342e1
Ilya Fomichev <fomil.music@gmail.com>
2023-07-05 12:31:58 +0500

    delete UnsupportedUriSchemeException SplitPackagesNotSupportedException docs

--- ackpine-core/src/main/kotlin/ru/solrudev/ackpine/exceptions/Exceptions.kt
+++ ackpine-core/src/main/kotlin/ru/solrudev/ackpine/exceptions/Exceptions.kt
@@ -1,12 +1,10 @@
 package ru.solrudev.ackpine.exceptions
 
-import android.net.Uri
 import android.os.Build
 
+/**
+ * Thrown if installation of split packages is not supported when creating session with split package is attempted.
+ */
 public class SplitPackagesNotSupportedException : IllegalArgumentException(
 	"Split packages are not supported on current Android API level: ${Build.VERSION.SDK_INT}"
-)
-
-public class UnsupportedUriSchemeException(uri: Uri) : IllegalArgumentException(
-	"Scheme of the provided URI is not supported: $uri"
 )
