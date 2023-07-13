9ba4ac3005ea979291d011c91ad8906ac12907ba
Ilya Fomichev <fomil.music@gmail.com>
2023-07-13 16:11:40 +0500

    mark Progress constructor with JvmOverloads

--- ackpine-core/src/main/kotlin/ru/solrudev/ackpine/session/Progress.kt
+++ ackpine-core/src/main/kotlin/ru/solrudev/ackpine/session/Progress.kt
@@ -3,7 +3,7 @@ package ru.solrudev.ackpine.session
 /**
  * Represents progress data.
  */
-public data class Progress(
+public data class Progress @JvmOverloads public constructor(
 	public val progress: Int = 0,
 	public val max: Int = 100
 )
