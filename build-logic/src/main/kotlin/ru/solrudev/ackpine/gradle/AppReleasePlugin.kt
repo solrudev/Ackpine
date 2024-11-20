df980177a7238be492de21de7531d9461951a539
Ilya Fomichev <fomil.music@gmail.com>
2024-11-20 15:30:14 +0500

    cache signing config read from file

--- build-logic/src/main/kotlin/ru/solrudev/ackpine/gradle/AppReleasePlugin.kt
+++ build-logic/src/main/kotlin/ru/solrudev/ackpine/gradle/AppReleasePlugin.kt
@@ -30,15 +30,16 @@ import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.Delete
 import org.gradle.api.tasks.TaskProvider
+import org.gradle.kotlin.dsl.assign
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.hasPlugin
 import org.gradle.kotlin.dsl.named
 import org.gradle.kotlin.dsl.register
+import org.gradle.kotlin.dsl.registerIfAbsent
 import ru.solrudev.ackpine.gradle.helpers.addOutgoingArtifact
 import ru.solrudev.ackpine.gradle.helpers.consumable
 import ru.solrudev.ackpine.gradle.helpers.getOrThrow
-import ru.solrudev.ackpine.gradle.helpers.properties
 import ru.solrudev.ackpine.gradle.helpers.withReleaseBuildType
 import java.io.File
 
@@ -59,10 +60,14 @@ public class AppReleasePlugin : Plugin<Project> {
 	}
 
 	private fun Project.configureSigning() = extensions.configure<ApplicationExtension> {
-		val keystoreConfigFile = isolated.rootProject.projectDirectory.file("keystore.properties")
-		val fileConfigProvider = providers.properties(keystoreConfigFile).map { properties ->
-			properties.filterKeys { it.startsWith(APP_SIGNING_PREFIX) }
-		}
+		val fileConfigProvider = gradle
+			.sharedServices
+			.registerIfAbsent("signing", PropertiesFileService::class) {
+				parameters.propertiesFile = isolated.rootProject.projectDirectory.file("keystore.properties")
+			}
+			.map { service ->
+				service.properties.filterKeys { it.startsWith(APP_SIGNING_PREFIX) }
+			}
 		val environmentConfigProvider = providers.environmentVariablesPrefixedBy(APP_SIGNING_PREFIX)
 		val releaseSigningConfig = releaseSigningConfigProvider(fileConfigProvider, environmentConfigProvider)
 		buildTypes.named("release") {
--- build-logic/src/main/kotlin/ru/solrudev/ackpine/gradle/helpers/PropertiesHelpers.kt
+++ build-logic/src/main/kotlin/ru/solrudev/ackpine/gradle/helpers/PropertiesHelpers.kt
@@ -16,24 +16,17 @@
 
 package ru.solrudev.ackpine.gradle.helpers
 
-import org.gradle.api.file.RegularFile
-import org.gradle.api.provider.Provider
-import org.gradle.api.provider.ProviderFactory
 import java.io.File
 import java.util.Properties
 
 /**
- * Creates a [Provider] whose value is a [Properties] map loaded from the [file].
- */
-internal fun ProviderFactory.properties(file: RegularFile): Provider<Map<String, String>> {
-	return fileContents(file).asText.map(String::readProperties)
-}
-
-/**
  * Returns [Properties] map loaded from the file.
  */
+@Suppress("UNCHECKED_CAST")
 internal fun File.readProperties(): Map<String, String> {
-	return readText().readProperties()
+	val properties = Properties()
+	inputStream().use(properties::load)
+	return properties as Map<String, String>
 }
 
 /**
@@ -43,11 +36,4 @@ internal fun Map<String, String>.getOrThrow(key: String): String {
 	val value = get(key)
 	check(!value.isNullOrEmpty()) { "$key was not provided" }
 	return value
-}
-
-@Suppress("UNCHECKED_CAST")
-private fun String.readProperties(): Map<String, String> {
-	val properties = Properties()
-	properties.load(reader())
-	return properties as Map<String, String>
 }
