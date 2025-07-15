a753b308e41b4c720f03935bb7515524352e0a39
Ilya Fomichev <fomil.music@gmail.com>
2025-07-15 22:00:55 +0500

    remove unused import

--- build-logic/src/main/kotlin/ru/solrudev/ackpine/gradle/AckpinePlugin.kt
+++ build-logic/src/main/kotlin/ru/solrudev/ackpine/gradle/AckpinePlugin.kt
@@ -21,7 +21,6 @@ import org.gradle.api.Project
 import org.gradle.api.tasks.Delete
 import org.gradle.api.tasks.Sync
 import org.gradle.api.tasks.TaskProvider
-import org.gradle.kotlin.dsl.assign
 import org.gradle.kotlin.dsl.named
 import org.gradle.kotlin.dsl.register
 import ru.solrudev.ackpine.gradle.app.AppReleasePlugin
